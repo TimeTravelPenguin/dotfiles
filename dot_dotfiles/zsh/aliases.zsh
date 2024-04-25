a1def1fb5085886405ea1296312c49de6c7d99b3
TimeTravelPenguin <35803538+TimeTravelPenguin@users.noreply.github.com>
2024-04-25 13:14:52 +1000

    Extended aliases

--- dot_dotfiles/zsh/aliases.zsh
+++ dot_dotfiles/zsh/aliases.zsh
@@ -1,6 +1,12 @@
-alias reload!='. ~/.zshrc'
-
+alias so='source ~/.zshrc'
 alias cls='clear' # Good 'ol Clear Screen command
 alias python="/usr/local/bin/python3"
 
 alias vimdiff='nvim -d'
+alias mc="mc --nosubshell"
+alias ls='exa -1 --color=auto --icons -s type --group-directories-first'
+alias -g ls='exa -1 --color=auto --icons -s type --group-directories-first'
+alias tree='exa -s type --group-directories-first --tree --icons --colour-scale'
+alias treeh='exa -s type --group-directories-first --tree --icons --colour-scale -L 2'
+alias gtree='exa --git-ignore -s type --group-directories-first --tree --icons --color-scale'
+alias gtreeh='exa --git-ignore -s type --group-directories-first --tree --icons --color-scale -L 2'
